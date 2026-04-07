07af03febd7263f23058669a7daeb2b8e6cb98a5
Shahaan <shahaanh3@gmail.com>
2026-04-07 21:12:53 +0530

    Cleanup temporary script files and redundant PDF

--- dump_nb.py
+++
@@ -1,9 +0,0 @@
-import json
-
-nb = json.load(open("Model_Training_Final.ipynb", "r", encoding="utf-8"))
-for i, c in enumerate(nb["cells"]):
-    ct = c["cell_type"]
-    print(f"=== Cell {i} ({ct}) ===")
-    for line in c["source"]:
-        print(line, end="")
-    print("\n")
